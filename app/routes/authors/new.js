a83e844a6f9c7956e622834b5a2a0429f1a8c8bc
daniele ferrari <eleinadinha@hotmail.com>
2017-05-07 17:44:58 -0300

    trying to add with relashionship

--- app/routes/authors/new.js
+++ app/routes/authors/new.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
 
+  // isNameValid: Ember.computed.empty('name'),
+  // isDisabled: Ember.computed.not('isNameValid'),
+
+
   model(){
     return this.store.createRecord('author');
   },
--- app/routes/books/new.js
+++ app/routes/books/new.js
@@ -2,16 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  // isTitleValid: Ember.computed.empty('title'),
+  // isPriceValid: Ember.computed.empty('price'),
+  // isPublisherValid: Ember.computed.empty('publisher'),
+  // isValid: Ember.computed.and('isTitleValid', 'isPriceValid','isPublisherValid'),
+  // isDisabled: Ember.computed.not('isValid'),
+
+
   model(){
   return Ember.RSVP.hash({
     books: this.store.createRecord('book'),
     authors: this.store.findAll('author')
+    // this.store.find('author', params)
+
     })
   },
 
   actions:
   {
     saveBook(book){
+    //   let name = this.get('author').peekRecord('name', params);
+    //   let books = this.get('book').createRecord('book', {
+    //   name: name
+    //   });
+    // //book.save();
       book.save().then(() => this.transitionTo('books'));
     }
   }
