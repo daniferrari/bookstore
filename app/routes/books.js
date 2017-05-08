c551302259f010fc3593c42b219f99079ae6f6c8
daniele_sciensa <daniele.melo@soaexpert.com.br>
2017-05-08 11:03:33 -0300

    Delete books.

--- app/routes/books.js
+++ app/routes/books.js
@@ -5,6 +5,15 @@ export default Ember.Route.extend({
 model(){
 
   return this.store.findAll('book');
+},
+
+actions: {
+  deleteBook(book) {
+    let confirmation = confirm('Are you sure?');
+    if (confirmation) {
+      book.destroyRecord();
+    }
+  }
 }
 
 });
